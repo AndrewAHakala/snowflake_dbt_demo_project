110219c4134450b8da1245c6f98c6187e9d31ea7
Andrew Hakala <andrew.hakala@fishtownanalytics.com>
2025-01-09 15:57:08 +0000

    Organizing folders and changing materialization types.

--- models/staging/stg_line_items.sql
+++ models/staging/stg_line_items.sql
@@ -1,8 +1,7 @@
 SELECT
+li.l_orderkey,
 li.l_linenumber,
     li.l_partkey,
     li.l_quantity,
     li.l_extendedprice
   FROM tpch_data.lineitem li
-
-  --FROM {{ source('tpch_data', 'lineitem') }} li
