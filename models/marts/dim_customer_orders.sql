9e2750e257600d49e324136469280571105a02e7
Andrew Hakala <andrew.hakala@fishtownanalytics.com>
2025-01-10 17:44:22 +0000

    Adding more staging models

--- models/marts/dim_customer_orders.sql
+++ models/marts/dim_customer_orders.sql
@@ -4,5 +4,5 @@ SELECT
     o.o_orderkey,
     o.o_orderdate,
     o.o_totalprice
-  FROM {{ source('tpch', 'customer') }} c
-    JOIN  {{ source('tpch', 'orders') }} o ON c.c_custkey = o.o_custkey
+  FROM {{ ref('stg_customer') }} c
+    JOIN  {{ ref('stg_orders') }} o ON c.c_custkey = o.o_custkey
