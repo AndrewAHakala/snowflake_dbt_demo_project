5fa86698005ecd72b6f9846cdf32670d75a86a1d
Andrew Hakala <andrew.hakala@fishtownanalytics.com>
2025-02-03 14:17:57 +0000

    Remove miultiply

--- models/marts/aggregates/monthly_gross_revenue.sql
+++ models/marts/aggregates/monthly_gross_revenue.sql
@@ -7,7 +7,7 @@
 select
     date_trunc(month, order_date) as order_month,
     region_name,
-    sum(gross_item_sales_amount)*10 as gross_revenue
+    sum(gross_item_sales_amount) as gross_revenue
     
 
 from {{ ref('fct_order_items') }}
